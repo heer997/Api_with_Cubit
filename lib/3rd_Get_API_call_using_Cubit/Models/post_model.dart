33bbafffb6f2359009a4ecbc26e201e32dcbbfad
Heer Nayakpara <heernayakpara997@gmail.com>
2024-06-04 14:25:41 +0530

    Initial

--- lib/3rd_Get_API_call_using_Cubit/Models/post_model.dart
+++ lib/3rd_Get_API_call_using_Cubit/Models/post_model.dart
@@ -5,14 +5,6 @@ class Post {
 
   Post({required this.id, required this.title, required this.body});
 
-  // Map<String, dynamic> toMap() {
-  //   return {
-  //     "id": this.id,
-  //     "title": this.title,
-  //     "body": this.body,
-  //   };
-  // }
-
   factory Post.fromMap(Map<String, dynamic> map) {
     return Post(
       id: map["id"] as int,
